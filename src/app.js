efbf3477b6884f1ee19404f5cd646d6197d0146d
mlorenzm <mlorenzomohamed@gmail.com>
2023-04-19 19:52:58 +0100

    removed test functionality

--- src/app.js
+++ src/app.js
@@ -1,40 +1,29 @@
-const dom = (() => {
-  // DOM
-  const startBtn = document.getElementById('start')
-  const main = document.getElementById('main')
-  const gameContainer = document.getElementById('game-container')
-  function toggleChildren() {
-    let children = main.children
+// This only serves a purpose: toggle between start screen and main game
+const displayController = (() => {
+  const startBtn = document.getElementById('start'),
+    main = document.getElementById('main')
+
+  toggleScreen = () => {
+    const children = main.children
     for (const child of main.children) {
       child.classList.toggle('hidden')
     }
   }
-  // Events
-  startBtn.addEventListener('click', toggleChildren)
-  startBtn.addEventListener('click', () => {
-    console.log('a')
-  })
 
-  return { toggleChildren: toggleChildren }
+  // Events
+
+  startBtn.addEventListener('click', toggleScreen)
+
+  return { toggleScreen: toggleScreen }
 })()
 
-// const Player = (symbol, array) => {
-//   const symb = symbol
-//   const pickCell = (array) => {
-//     for (i of array) {
-//       console.log(i)
-//     }
-//   }
-//   return { pickCell: pickCell() }}
-// const p1 = Player('circle', dom.cells)
-// Factory para players
-
-//Factory para games
+// Factory for players
+const playerFactory = (symbol) => {
+  symbol = symbol
+  const getSymbol = () => {
+    return symbol
+  }
+  return { getSymbol }
+}
 
 //displayController??
-
-const gameBoard = (() => {
-  let board = dom.cells
-  function renderBoard() {}
-  return { board: board }
-})()
