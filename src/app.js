2b57fcaf80146e7195161fd3d44baa76b9607dd5
mlorenzm <mlorenzomohamed@gmail.com>
2023-04-20 18:54:59 +0100

    gameBoard getBoard and placetoken methods

--- src/app.js
+++ src/app.js
@@ -1,29 +1,66 @@
-// This only serves a purpose: toggle between start screen and main game
-const displayController = (() => {
-  const startBtn = document.getElementById('start'),
-    main = document.getElementById('main')
+// const displayController = (() => {
+//   const startBtn = document.getElementById('start'),
+//     main = document.getElementById('main'),
+//     submitBtn = document.getElementById('start-game')
 
-  toggleScreen = () => {
-    const children = main.children
-    for (const child of main.children) {
-      child.classList.toggle('hidden')
+//   // document.querySelector("input[type='radio'][name=rate]:checked").value
+//   // Functions
+//   toggleScreen = () => {
+//     const children = main.children
+//     for (const child of main.children) {
+//       child.classList.toggle('hidden')
+//     }
+//   }
+//   createPlayers = (event) => {
+//     p1 = playerFactory(
+//       document.querySelector("input[type='radio'][name=p1-symbol]:checked")
+//         .value
+//     )
+//     p2 = playerFactory(
+//       document.querySelector("input[type='radio'][name=p2-symbol]:checked")
+//         .value
+//     )
+//   }
+//   // Events
+//   submitBtn.addEventListener('click', createPlayers)
+//   startBtn.addEventListener('click', toggleScreen)
+
+//   return { toggleScreen: toggleScreen, createPlayers: createPlayers }
+// })()
+
+//Gameboard, represents the state of the board
+const gameBoard = (() => {
+  // our board is an array of 3x3
+  board = ['', '', '', '', '', '', '', '', '']
+  // we need a method to export our board
+  const getBoard = () => {
+    return board
+  }
+  // we need a method to modify our board with current player's token on board's[index] position
+  const placeToken = (board, player, index) => {
+    // Here, we first need to see which positions are available (empty) on our array
+    if (board[index] === '') {
+      board[index] = player.getToken()
+    } else {
+      return
     }
   }
-
-  // Events
-
-  startBtn.addEventListener('click', toggleScreen)
-
-  return { toggleScreen: toggleScreen }
+  return { getBoard, placeToken }
 })()
 
-// Factory for players
-const playerFactory = (symbol) => {
-  symbol = symbol
-  const getSymbol = () => {
-    return symbol
+// Factory function for creating new players
+const playerFactory = (name, token) => {
+  name = name
+  token = token
+  const getToken = () => {
+    return token
   }
-  return { getSymbol }
+  const getName = () => {
+    return name
+  }
+  return { getToken, getName }
 }
-
-//displayController??
+player1 = playerFactory('a', 'x')
+player2 = playerFactory('a', '0')
+// Controls game flow
+const gameController = (player1, player2) => {}
